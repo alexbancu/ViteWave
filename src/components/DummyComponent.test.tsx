de4eda01042af7846a3b6ca6799b1b5105dc8386
bancualex <alexbancu95@gmail.com>
2023-05-10 13:01:12 +0300

    fix broken test

--- src/components/DummyComponent.test.tsx
+++ src/components/DummyComponent.test.tsx
@@ -4,7 +4,5 @@ import DummyComponent from "./DummyComponent";
 test("renders DummyComponent", () => {
   render(<DummyComponent />);
   const linkElement = screen.getByText(/Dummy Component/i);
-  const linkElement2 = screen.getByText(/Dummy Componentt/i);
   expect(linkElement).toBeInTheDocument();
-  expect(linkElement2).toBeInTheDocument();
 });
